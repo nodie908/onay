ea6577372bd7248d5c9c7219c103d40958468476
nodie908 <nikitios641@gmail.com>
2023-11-10 22:50:01 +0600

    start

---
+++ components/CardScreen.js
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import { View } from "react-native-web"
+
+const CardScreen = () => {
+    return (
+        <View>
+            <Text>Card screen</Text>
+        </View>
+    );
+}
+
+export default CardScreen;
---
+++ components/RoutesScreen.js
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import { View } from "react-native-web"
+
+const Details = () => {
+    return (
+        <View>
+            <Text>Routes screen</Text>
+        </View>
+    );
+}
+
+export default Details;
